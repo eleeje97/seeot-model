1ae018a6a3e6c61a7dfb5ac5bdb101436f2df10f
danalee <eleeje97@gmail.com>
2022-08-29 16:05:06 +0000

    human_parsing init

---
+++ human_parsing/run.py
@@ -0,0 +1,18 @@
+def human_parsing(img_path):
+    # copy img
+
+    # put image in inputs/
+
+    # run image parsing
+
+    # return output img_path
+
+    return
+
+
+def remove_images():
+    # remove images in inputs/
+    # remove images in outputs/
+
+    return
+
